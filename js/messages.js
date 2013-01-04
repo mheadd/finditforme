a684b2539b9c5dfd327f01335d45fe29858b92fe
Mark Headd <mheadd@gmail.com>
2013-01-03 19:32:11 -0500

    Changed SMS number in message.

--- js/messages.js
+++ js/messages.js
@@ -1,7 +1,7 @@
 // Object with message components.
 var messages = {
     butonHTML: '<button type="button" class="close" data-dismiss="alert">×</button>',
-    sms: 'You can also find a place by sending a text message to <strong>(267) 293-9385</strong>. Send an address in Philly with a place type, like "908 N. 3rd Street, Philadelphia PA #bakery"',
+    sms: 'You can also find a place by sending a text message to <strong>(215) 600-2137</strong>. Send an address in Philly with a place type, like "908 N. 3rd Street, Philadelphia PA #bakery"',
     about: 'PhindItFor.me is a demo application built by <a href="https://twitter.com/mheadd">Mark Headd</a> as an example of how to use the Google Places API. more information <a href="http://codeforamerica.org/2012/10/05/calling-all-civic-coders/">here</a>.'
 };
 
